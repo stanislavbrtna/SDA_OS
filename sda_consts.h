c1e3be1e0f86509b53e57b30b946f0674f8639a8
standa <stanislav.brtna@gmail.com>
2019-02-05 22:20:23 +0100

    Beta designation removed.

--- sda_consts.h
+++ sda_consts.h
@@ -28,7 +28,7 @@ File description:
 #define SDA_CONSTS_H
 
 // version number string and version number,
-#define SDA_OS_VERSION "0.7.2.0B"
+#define SDA_OS_VERSION "0.7.2.0"
 #define SDA_OS_VERSION_NUM 720
 
 // shows where the screen is redrawn, works only in simulator, usefull for debug
