3e64d1cd7e93622397c60481e307eaabe0ae85ac
standa <stanislav.brtna@gmail.com>
2019-11-17 18:26:49 +0100

    Now out of beta.

--- sda_consts.h
+++ sda_consts.h
@@ -28,7 +28,7 @@ File description:
 #define SDA_CONSTS_H
 
 // version number string and version number,
-#define SDA_OS_VERSION "0.8.0.0B"
+#define SDA_OS_VERSION "0.8.0.0"
 #define SDA_OS_VERSION_NUM 800
 
 // shows where the screen is redrawn, works only in simulator, usefull for debug
