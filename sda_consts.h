bf73e1234ec4141e7c30160f26bf096e78f11302
standa <stanislav.brtna@gmail.com>
2021-08-27 00:42:47 +0200

    pushed to rc

--- sda_consts.h
+++ sda_consts.h
@@ -28,7 +28,7 @@ File description:
 #define SDA_CONSTS_H
 
 // version number string and version number,
-#define SDA_OS_VERSION "1.0.0.0B"
+#define SDA_OS_VERSION "1.0.0.0RC1"
 #define SDA_OS_VERSION_NUM 1000
 
 // shows where the screen is redrawn, works only in simulator, usefull for debug
