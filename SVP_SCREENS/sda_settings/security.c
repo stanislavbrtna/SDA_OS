5dbb1c346b4d8fc8a54be159326c3e24a44dd557
standa <stanislav.brtna@gmail.com>
2023-04-17 20:55:28 +0200

    layout fixed, cs fixed

--- SVP_SCREENS/sda_settings/security.c
+++ SVP_SCREENS/sda_settings/security.c
@@ -34,9 +34,9 @@ uint16_t sda_settings_security_screen(uint8_t init) {
   static uint8_t optSecuNewStr[33];
   static uint8_t optSecuOldStr[33];
   static uint16_t optSecuOk;
-  static uint16_t optSecuMsg;
-  static uint16_t optSecuMsg2;
-  static uint16_t optSecuMsg3;
+  static uint16_t msgWrongPwd;
+  static uint16_t msgPwdStored;
+  static uint16_t msgKeyMismatch;
   static uint16_t optSecuLock;
   uint16_t optSecuScr;
 
@@ -51,20 +51,21 @@ uint16_t sda_settings_security_screen(uint8_t init) {
     optSecuNew = gr2_add_text(1, 6, 8, 7, (uint8_t *)"", optSecuScr, &sda_sys_con);
     optSecuOldBtn = gr2_add_button(8, 4, 9, 5, (uint8_t *)"*", optSecuScr, &sda_sys_con);
     optSecuNewBtn = gr2_add_button(8, 6, 9, 7, (uint8_t *)"*", optSecuScr, &sda_sys_con);
-    optSecuLock = gr2_add_button(3, 10, 9, 11, SCR_LOCK_DEVICE, optSecuScr, &sda_sys_con);
-    optSecuOk = gr2_add_button(4, 8, 9, 9, SCR_CHANGE_PASSWORD, optSecuScr, &sda_sys_con);
-    optSecuMsg = gr2_add_text(1, 7, 9, 8, SCR_WRONG_PASSWORD, optSecuScr, &sda_sys_con);
-    optSecuMsg2 = gr2_add_text(1, 7, 9, 8, SCR_PASSWORD_STORED, optSecuScr, &sda_sys_con);
-    optSecuMsg3 = gr2_add_text(0, 7, 10, 8, SCR_KEY_MISMATCH, optSecuScr, &sda_sys_con);
+    optSecuLock = gr2_add_button(2, 10, 8, 11, SCR_LOCK_DEVICE, optSecuScr, &sda_sys_con);
+    optSecuOk = gr2_add_button(2, 8, 8, 9, SCR_CHANGE_PASSWORD, optSecuScr, &sda_sys_con);
+    msgWrongPwd = gr2_add_text(1, 7, 9, 8, SCR_WRONG_PASSWORD, optSecuScr, &sda_sys_con);
+    msgPwdStored = gr2_add_text(1, 7, 9, 8, SCR_PASSWORD_STORED, optSecuScr, &sda_sys_con);
+    msgKeyMismatch = gr2_add_text(0, 7, 10, 8, SCR_KEY_MISMATCH, optSecuScr, &sda_sys_con);
 
-    gr2_text_set_align(optSecuMsg3, GR2_ALIGN_CENTER, &sda_sys_con);
+    gr2_text_set_align(msgKeyMismatch, GR2_ALIGN_CENTER, &sda_sys_con);
 
-    resetBtn = gr2_add_button(1, 9, 9, 10, SCR_RESET_KEY, optSecuScr, &sda_sys_con);
+    resetBtn = gr2_add_button(2, 9, 8, 10, SCR_RESET_KEY, optSecuScr, &sda_sys_con);
     gr2_text_set_align(resetBtn, GR2_ALIGN_CENTER, &sda_sys_con);
+    gr2_text_set_align(optSecuLock, GR2_ALIGN_CENTER, &sda_sys_con);
     gr2_set_visible(resetBtn, 0, &sda_sys_con);
 
-    gr2_text_set_align(optSecuMsg, GR2_ALIGN_RIGHT, &sda_sys_con);
-    gr2_text_set_align(optSecuMsg2, GR2_ALIGN_RIGHT, &sda_sys_con);
+    gr2_text_set_align(msgWrongPwd, GR2_ALIGN_RIGHT, &sda_sys_con);
+    gr2_text_set_align(msgPwdStored, GR2_ALIGN_RIGHT, &sda_sys_con);
 
     gr2_text_set_editable(optSecuNew, 1, &sda_sys_con);
     gr2_text_set_editable(optSecuOld, 1, &sda_sys_con);
@@ -72,7 +73,7 @@ uint16_t sda_settings_security_screen(uint8_t init) {
     gr2_text_set_pwd(optSecuNew, 1, &sda_sys_con);
     gr2_text_set_pwd(optSecuOld, 1, &sda_sys_con);
 
-    optSecuBack = gr2_add_button(7, 12, 9, 13, SCR_BACK, optSecuScr, &sda_sys_con);
+    optSecuBack = gr2_add_button(1, 12, 4, 13, SCR_BACK, optSecuScr, &sda_sys_con);
 
     gr2_text_set_align(optSecuBack, GR2_ALIGN_CENTER, &sda_sys_con);
     gr2_text_set_align(optSecuOk, GR2_ALIGN_CENTER, &sda_sys_con);
@@ -83,9 +84,9 @@ uint16_t sda_settings_security_screen(uint8_t init) {
   if (init == 2) {
     optSecuNewStr[0] = 0;
     optSecuOldStr[0] = 0;
-    gr2_set_visible(optSecuMsg, 0, &sda_sys_con);
-    gr2_set_visible(optSecuMsg2, 0, &sda_sys_con);
-    gr2_set_visible(optSecuMsg3, 0, &sda_sys_con);
+    gr2_set_visible(msgWrongPwd, 0, &sda_sys_con);
+    gr2_set_visible(msgPwdStored, 0, &sda_sys_con);
+    gr2_set_visible(msgKeyMismatch, 0, &sda_sys_con);
 
     if (svp_crypto_get_if_set_up() == 0) {
       gr2_set_grayout(optSecuOld, 1, &sda_sys_con);
@@ -119,7 +120,7 @@ uint16_t sda_settings_security_screen(uint8_t init) {
   if (gr2_clicked(resetBtn, &sda_sys_con)) {
     svp_crypto_reset_os_keyfile();
     gr2_set_visible(resetBtn, 0, &sda_sys_con);
-    gr2_set_visible(optSecuMsg3, 0, &sda_sys_con);
+    gr2_set_visible(msgKeyMismatch, 0, &sda_sys_con);
   }
 
   if (gr2_clicked(optSecuLock, &sda_sys_con)) {
@@ -137,8 +138,9 @@ uint16_t sda_settings_security_screen(uint8_t init) {
     }
     if (retval != 0) {
       if (retval == 2) {
-        gr2_set_visible(optSecuMsg, 1, &sda_sys_con);
-        gr2_set_visible(optSecuMsg2, 0, &sda_sys_con);
+        gr2_set_visible(msgWrongPwd, 1, &sda_sys_con);
+        gr2_set_visible(msgPwdStored, 0, &sda_sys_con);
+        gr2_set_visible(msgKeyMismatch, 0, &sda_sys_con);
       } else if(retval == 3) {
         gr2_set_grayout(optSecuOk, 1, &sda_sys_con);
         gr2_set_grayout(optSecuNew, 1, &sda_sys_con);
@@ -147,8 +149,8 @@ uint16_t sda_settings_security_screen(uint8_t init) {
     } else {
       gr2_set_grayout(optSecuOld, 0, &sda_sys_con);
       gr2_set_grayout(optSecuLock, 0, &sda_sys_con);
-      gr2_set_visible(optSecuMsg2, 1, &sda_sys_con);
-      gr2_set_visible(optSecuMsg, 0, &sda_sys_con);
+      gr2_set_visible(msgPwdStored, 1, &sda_sys_con);
+      gr2_set_visible(msgWrongPwd, 0, &sda_sys_con);
       if (svp_crypto_get_if_set_up()){
         svp_crypto_change_password(optSecuNewStr);
         svp_crypto_reencrypt_os_keyfile(optSecuOldStr, optSecuNewStr);
@@ -157,7 +159,7 @@ uint16_t sda_settings_security_screen(uint8_t init) {
       }    
       if (sda_crypto_keyfile_init_check() != 0) {
         sda_show_error_message(SCR_KEY_ERROR_MSG);
-        gr2_set_visible(optSecuMsg3, 1, &sda_sys_con);
+        gr2_set_visible(msgKeyMismatch, 1, &sda_sys_con);
         gr2_set_visible(resetBtn, 1, &sda_sys_con);
       }
       svp_crypto_lock();
