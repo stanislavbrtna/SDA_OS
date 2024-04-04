c0d7417f06a1c7ca0cbfd3cad67b9d44c3c5d7e0
standa <stanislav.brtna@gmail.com>
2024-04-04 11:33:09 +0200

    text handler separated from wrapper

--- SVS_WRAP/os_wrapper/sda_os_gui.c
+++ SVS_WRAP/os_wrapper/sda_os_gui.c
@@ -62,8 +62,147 @@ int32_t get_char_cursor_pos(uint16_t cpos, uint8_t *str) {
 }
 
 
+uint8_t svm_text_handler(varRetVal *result, argStruct *argS, svsVM *s) {
+  uint32_t x = 0;
+
+  // set default return
+  result->value = argS->arg[2];
+  result->type = SVS_TYPE_STR;
+
+  // text not active
+  if (!gr2_get_value(argS->arg[1].val_s, &sda_app_con)) {
+    return 1;
+  }
+
+  if(gr2_cursor_handler(argS->arg[1].val_s, svpSGlobal.uptimeMs, &sda_app_con)) {
+    sda_clipboard_overlay_init(argS->arg[1].val_s);
+    return 1;
+  }
+
+  if (svpSGlobal.newStringIdFlag == argS->arg[1].val_s) {
+    result->value.val_str = svpSGlobal.newString;
+    svpSGlobal.newStringIdFlag = 0;
+    result->type = SVS_TYPE_STR;
+    return 1;
+  }
+
+  // keyboard read and svs string modification
+  if (sda_get_keyboard_key_flag()) {
+    
+    // block rewrite handling
+    if (sda_app_con.textBlockStart != 0 || sda_app_con.textBlockEnd != 0) {
+      strNewStreamInit(s);
+      uint32_t i = 0;
+      uint8_t * str = gr2_get_str((uint16_t)argS->arg[1].val_str, sda_current_con);
+      
+      while(str[i] != 0) {
+        if (!(i >= (sda_current_con->textBlockStart - 1) && i < sda_current_con->textBlockEnd)) {
+          strNewStreamPush(str[i], s);
+        }
+        i++;
+      }
+      gr2_set_param((uint16_t)argS->arg[1].val_str, sda_current_con->textBlockStart - 1, sda_current_con);
+
+      argS->arg[2].val_str = strNewStreamEnd(s);
+      sda_app_con.textBlockStart = 0;
+      sda_app_con.textBlockEnd = 0;
+
+      // backspace/delete
+      if (svpSGlobal.kbdKeyStr[0] == 8) {
+          result->value = argS->arg[2];
+          result->type = SVS_TYPE_STR;
+          return 1; 
+      }
+    }
+
+    if ((svpSGlobal.kbdKeyStr[0]) != 8) { // normal chars
+      result->value.val_str
+        = strInsert(
+                    (uint16_t)argS->arg[2].val_str,
+                    strNew(svpSGlobal.kbdKeyStr, s),
+                    gr2_get_param(argS->arg[1].val_s, &sda_app_con),
+                    s
+          ); // add char/string
+      
+
+      gr2_set_param(
+        argS->arg[1].val_s,
+        gr2_get_param(argS->arg[1].val_s, &sda_app_con) + sda_strlen(svpSGlobal.kbdKeyStr),
+        &sda_app_con
+      ); // move cursor
+
+      result->type = SVS_TYPE_STR;
+      return 1;
+    } else { // delete/backspace
+      uint16_t len = 0;
+      uint16_t prac;
+      uint16_t charIndex;
+      uint8_t czFlag = 0;
+
+      while(*(s->stringField + argS->arg[2].val_str + len) != 0) {
+        len++;
+      }
+
+      result->value.val_str = strNew(s->stringField + argS->arg[2].val_str, s); // create new string
+
+      if ((svpSGlobal.kbdKeyStr[1]) != 'd') { // backspace
+        if (len > 0 && gr2_get_param(argS->arg[1].val_s, &sda_app_con) != 0) {
+          charIndex = result->value.val_str + gr2_get_param(argS->arg[1].val_s, &sda_app_con);
+
+          if (len >= 2
+              && (s->stringField[charIndex - 2] >= 0xC3)
+              && (s->stringField[charIndex - 2] <= 0xC5))
+          {
+            gr2_set_param(argS->arg[1].val_s, gr2_get_param(argS->arg[1].val_s, &sda_app_con) - 2, &sda_app_con);
+            czFlag = 1;
+          } else {
+            gr2_set_param(argS->arg[1].val_s, gr2_get_param(argS->arg[1].val_s, &sda_app_con) - 1, &sda_app_con);
+          }
+
+        } else {
+          result->value = argS->arg[2];
+          result->type = SVS_TYPE_STR;
+          return 1;
+        }
+      } else { // delete
+        if (len > 0 && len != gr2_get_param(argS->arg[1].val_s, &sda_app_con)) {
+          charIndex = result->value.val_str + gr2_get_param(argS->arg[1].val_s, &sda_app_con);
+
+          if (len >= 2
+              && (s->stringField[charIndex] >= 0xC3)
+              && (s->stringField[charIndex] <= 0xC5))
+          {
+            czFlag = 1;
+          }
+
+        } else {
+          result->value = argS->arg[2];
+          result->type = SVS_TYPE_STR;
+          return 1;
+        }
+      }
+
+      prac = gr2_get_param(argS->arg[1].val_s, &sda_app_con);
+      x = 0;
+
+      while(s->stringField[result->value.val_str + prac + x] != 0) {
+        s->stringField[result->value.val_str + prac + x]
+          = s->stringField[result->value.val_str + prac + x + 1 + czFlag];
+        x++;
+      }
+
+      *((uint8_t*)(s->stringField + result->value.val_str + len - 1)) = 0; //zkrátíme pro jistotu
+      gr2_set_str(argS->arg[1].val_s,s->stringField + result->value.val_str, &sda_app_con); //nastavíme text/updatujeme
+      result->type = SVS_TYPE_STR;
+      return 1;
+    }
+  }
+
+  return 1;
+}
+
+
 uint8_t sda_os_gui_wrapper(varRetVal *result, argStruct *argS, svsVM *s) {
-  uint16_t x;
   uint8_t argType[11];
 
   //#!#### Gui
@@ -157,147 +296,13 @@ uint8_t sda_os_gui_wrapper(varRetVal *result, argStruct *argS, svsVM *s) {
       return 0;
     }
 
-    x = 0;
-    while(*(s->stringField + argS->arg[2].val_str + x) != 0) {
-      x++;
-    }
-
-    if (gr2_get_value(argS->arg[1].val_s, &sda_app_con)) {
-
-      if(gr2_cursor_handler(argS->arg[1].val_s, svpSGlobal.uptimeMs, &sda_app_con)) {
-        sda_clipboard_overlay_init(argS->arg[1].val_s);
-        result->value = argS->arg[2];
-        result->type = SVS_TYPE_STR;
-        return 1;
-      }
-
-      if (svpSGlobal.newStringIdFlag == argS->arg[1].val_s) {
-        result->value.val_str = svpSGlobal.newString;
-        svpSGlobal.newStringIdFlag = 0;
-        result->type = SVS_TYPE_STR;
-        return 1;
-      }
-
-      // keyboard read and svs string modification
-      if (sda_get_keyboard_key_flag()) {
-        
-        // block rewrite handling
-        if (sda_app_con.textBlockStart != 0 || sda_app_con.textBlockEnd != 0) {
-          strNewStreamInit(s);
-          uint32_t i = 0;
-          uint8_t * str = gr2_get_str((uint16_t)argS->arg[1].val_str, sda_current_con);
-          
-          while(str[i] != 0) {
-            if (!(i >= (sda_current_con->textBlockStart - 1) && i < sda_current_con->textBlockEnd)) {
-              strNewStreamPush(str[i], s);
-            }
-            i++;
-          }
-          gr2_set_param((uint16_t)argS->arg[1].val_str, sda_current_con->textBlockStart - 1, sda_current_con);
-
-          argS->arg[2].val_str = strNewStreamEnd(s);
-          sda_app_con.textBlockStart = 0;
-          sda_app_con.textBlockEnd = 0;
-
-          // backspace/delete
-          if (svpSGlobal.kbdKeyStr[0] == 8) {
-              result->value = argS->arg[2];
-              result->type = SVS_TYPE_STR;
-              return 1; 
-          }
-        }
-
-        if ((svpSGlobal.kbdKeyStr[0]) != 8) { // normal chars
-          result->value.val_str
-            = strInsert(
-                        (uint16_t)argS->arg[2].val_str,
-                        strNew(svpSGlobal.kbdKeyStr, s),
-                        gr2_get_param(argS->arg[1].val_s, &sda_app_con),
-                        s
-              ); // add char/string
-          
-
-          gr2_set_param(
-            argS->arg[1].val_s,
-            gr2_get_param(argS->arg[1].val_s, &sda_app_con) + sda_strlen(svpSGlobal.kbdKeyStr),
-            &sda_app_con
-          ); // move cursor
-
-          result->type = SVS_TYPE_STR;
-          return 1;
-        } else { // delete/backspace
-          uint16_t len = 0;
-          uint16_t prac;
-          uint16_t charIndex;
-          uint8_t czFlag = 0;
-
-          while(*(s->stringField + argS->arg[2].val_str + len) != 0) {
-            len++;
-          }
-
-          result->value.val_str = strNew(s->stringField + argS->arg[2].val_str, s); // create new string
-
-          if ((svpSGlobal.kbdKeyStr[1]) != 'd') { // backspace
-            if (len > 0 && gr2_get_param(argS->arg[1].val_s, &sda_app_con) != 0) {
-              charIndex = result->value.val_str + gr2_get_param(argS->arg[1].val_s, &sda_app_con);
-
-              if (len >= 2
-                  && (s->stringField[charIndex - 2] >= 0xC3)
-                  && (s->stringField[charIndex - 2] <= 0xC5))
-              {
-                gr2_set_param(argS->arg[1].val_s, gr2_get_param(argS->arg[1].val_s, &sda_app_con) - 2, &sda_app_con);
-                czFlag = 1;
-              } else {
-                gr2_set_param(argS->arg[1].val_s, gr2_get_param(argS->arg[1].val_s, &sda_app_con) - 1, &sda_app_con);
-              }
-
-            } else {
-              result->value = argS->arg[2];
-              result->type = SVS_TYPE_STR;
-              return 1;
-            }
-          } else { // delete
-            if (len > 0 && len != gr2_get_param(argS->arg[1].val_s, &sda_app_con)) {
-              charIndex = result->value.val_str + gr2_get_param(argS->arg[1].val_s, &sda_app_con);
-
-              if (len >= 2
-                  && (s->stringField[charIndex] >= 0xC3)
-                  && (s->stringField[charIndex] <= 0xC5))
-              {
-                czFlag = 1;
-              }
-
-            } else {
-              result->value = argS->arg[2];
-              result->type = SVS_TYPE_STR;
-              return 1;
-            }
-          }
-
-          prac = gr2_get_param(argS->arg[1].val_s, &sda_app_con);
-          x = 0;
-
-          while(s->stringField[result->value.val_str + prac + x] != 0) {
-            s->stringField[result->value.val_str + prac + x]
-              = s->stringField[result->value.val_str + prac + x + 1 + czFlag];
-            x++;
-          }
-
-          *((uint8_t*)(s->stringField + result->value.val_str + len - 1)) = 0; //zkrátíme pro jistotu
-          gr2_set_str(argS->arg[1].val_s,s->stringField + result->value.val_str, &sda_app_con); //nastavíme text/updatujeme
-          result->type = SVS_TYPE_STR;
-          return 1;
-        }
-      }
-    }
-
+    svm_text_handler(result, argS, s);
+    
     // when string differs from the param, the parameter string is set again
     if (gr2_get_str(argS->arg[1].val_s, &sda_app_con) != (uint8_t *) ((uint32_t)s->stringField + (uint32_t)argS->arg[2].val_str)) {
       gr2_set_str(argS->arg[1].val_s, s->stringField + argS->arg[2].val_str, &sda_app_con);
     }
-    
-    result->value = argS->arg[2];
-    result->type = SVS_TYPE_STR;
+
     return 1;
   }
 
